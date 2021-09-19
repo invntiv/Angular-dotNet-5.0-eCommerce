d123bd1dbe4186d7bd660c18c28b6fe9324ccccf
Josh Mack <mack.joshuaj@gmail.com>
2021-09-18 21:49:59 -0500

    app.component.ts updates

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-declare var jQuery: any;
 
 @Component({
   selector: 'app-root',
@@ -13,13 +12,15 @@ export class AppComponent implements OnInit {
   // Angular Style conventions:
   // Class Properties
   title = 'eCommerce'; 
+  products: any[] | undefined;
 
   //Constructor
   constructor(private http: HttpClient) {}
 
   // Lifecycle Hooks
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products').subscribe((response: any) => {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: any) => {
+      this.products = response.data;
       console.log(response);
     }, error => {
       console.log(error);
